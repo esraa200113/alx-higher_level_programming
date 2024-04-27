e1415b4589b48df0d4e375b558ac2bf48b688439
esraa200113 <esraa200113@gmail.com>
2024-04-27 03:30:25 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,19 @@
 # models/base.py
+"""
+This module contains the implementation of the Base class.
+"""
+
 class Base:
+    """Base class for managing IDs."""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes a Base instance.
+
+        Args:
+            id (int, optional): ID of the instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
