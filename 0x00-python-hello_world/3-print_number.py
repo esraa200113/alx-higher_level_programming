4fcf0689e8d3f6bc33dfe8fed0eadf4ed28273c1
esraa200113 <esraa200113@gmail.com>
2024-05-04 03:07:41 +0300

    4-print_float.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This script prints the integer stored in the variable number, followed by Battery street, followed by a new line.
+"""
+
 number = 98
-print(f"{number} Battery street\n")
+print(f"{number:d} Battery street")
+
 
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+This script prints the float stored in the variable number with a precision of 2 digits.
+"""
+
+number = 3.14159
+print(f"Float: {number:.2f}")
+
